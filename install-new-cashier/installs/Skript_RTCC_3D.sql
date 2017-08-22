cb2c15295ef9b9f44b915bb56033dc362293116b
henmanuel <admin@loopzone.org>
2017-08-22 09:47:55 -0600

    Rest System

--- install-new-cashier/installs/Skript_RTCC_3D.sql
+++
@@ -1,7 +0,0 @@
-/*Update caProcessor*/
-UPDATE caProcessor SET AllowsRefunds=1 WHERE caProcessor_Id = 794;
-
-/*insert caProcessorSetting_Detail*/
-INSERT INTO caProcessorSetting_Detail (caProcessorSetting_Group_Id, Code, Value, Label, Length, Description) 
-VALUES (134, 'URL_REFUND', 'https://secure.qwipi.com/universalS2S/refund', NULL, NULL, NULL),
-       (134, 'REFUND_RETURN_URL', 'https://public.secureprivate.com/rtcc/status/', NULL, NULL, NULL);
