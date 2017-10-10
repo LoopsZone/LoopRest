4f475278815ec06a8dc85d8d202f196b86ec5c57
henmanuel <admin@loopzone.org>
2017-10-10 11:23:41 -0600

    Loop Rest Views

--- views/Views.class.php
+++ views/Views.class.php
@@ -1,10 +1,10 @@
 <?php
 
 class Views{
+    private $currentComponent;
     const COMPONENTS_DIR = 'views' . DIRECTORY_SEPARATOR . 'staticFiles';
 
-    function __construct($request)
-    {
+    function __construct($request) {
         $model = Model::getInstance();
         $viewMD = $model->getViewsInstance();
         $target = ($request) ? $request : GlobalConstants::PRINCIPAL_VIEW;
@@ -33,57 +33,60 @@ class Views{
             $componentHTML = DIRECTORY . self::COMPONENTS_DIR . DIRECTORY_SEPARATOR . $view . DIRECTORY_SEPARATOR . "{$parent}.html";
 
             if (file_exists($componentHTML)) {
-
-                $matchesComponents = array();
                 $currentComponent = file_get_contents($componentHTML);
-                $render = preg_match_all('/<comp>[a-zA-Z0-9_\-]+<\/comp>/', $currentComponent, $matchesComponents, PREG_PATTERN_ORDER);
+                $render = preg_match_all('/<comp[\s]*name=["|\'][a-zA-Z0-9_\-]*["|\']+[\s]*\/>/', $currentComponent, $matchesComponents, PREG_PATTERN_ORDER);
 
                 if ($render) {
 
-                    $resource = preg_match_all('/<src type=["|\'](img|js|css|file)["|\']>[a-zA-Z0-9_\-]+<\/src>/', $currentComponent, $matchesResources, PREG_PATTERN_ORDER);
-
-                    if ($resource) {
-
-                        $clear[] = '';
-                        $clear[] = '';
-                        $patrons[] = '/<src type=["|\'](img|js|css|file)["|\']>/';
-                        $patrons[] = '/<\/src>/';
-
-                        $resource = preg_replace($patrons, $clear, $matchesResources[0]);
-                        $types = $matchesResources[1];
-                        $count = count($resource);
-
-                        for ($i = 0; $i < $count; $i++) {
-                            $resourcesPath = self::COMPONENTS_DIR . DIRECTORY_SEPARATOR . $view . DIRECTORY_SEPARATOR . 'Resources' . DIRECTORY_SEPARATOR . $types[$i] . DIRECTORY_SEPARATOR;
-                            $resources = scandir($resourcesPath);
-                            $file = $resource[$i].".{$types[$i]}";
-                            $found = in_array($file, $resources);
-
-                            if ($found){
-                                $path = "'{$resourcesPath}{$file}'";
-                                $currentComponent = str_replace($matchesResources[0][$i], $path,$currentComponent);
-                            }
-                        }
-                    }
-
                     $clear[] = '';
                     $clear[] = '';
-                    $patrons[] = '/<comp>/';
-                    $patrons[] = '/<\/comp>/';
+                    $patrons[] = '/<comp[\s]*name=["|\']/';
+                    $patrons[] = '/["|\']+[\s]*\/>/';
 
                     $components = preg_replace($patrons, $clear, $matchesComponents[0]);
                     $count = count($components);
 
                     for ($i = 0; $i < $count; $i++) {
                         $renderedComponent = $this->render($view, $components[$i]);
-                        $currentComponent = str_replace($matchesComponents[0][$i], $renderedComponent,$currentComponent);
+                        $this->currentComponent = str_replace($matchesComponents[0], $renderedComponent,$currentComponent);
                     }
                 }
 
-                return $currentComponent;
+                $this->resources($view);
+
+                return $this->currentComponent;
             }
         }
 
         return false;
+    }
+
+    private function resources($view) {
+        $matchesResources = array();
+        $resource = preg_match_all('/<src[\s]*type=["|\'](js|css|png|gif|file)[\s]*["|\']*[\s]*name=["|\'][a-zA-Z0-9_\-]+["|\'][\s]*\/>/', $this->currentComponent, $matchesResources, PREG_PATTERN_ORDER);
+
+        if ($resource) {
+
+            $clear[] = '';
+            $clear[] = '';
+            $namePatrons[] = '/<src[\s]*type=["|\'](js|css|png|gif|file)[\s]*["|\']*[\s]*name=["|\']/';
+            $namePatrons[] = '/["|\'][\s]*\/>/';
+
+            $nameResource = preg_replace($namePatrons, $clear, $matchesResources[0]);
+
+            $count = count($nameResource);
+
+            for ($i = 0; $i < $count; $i++) {
+                $resourcesPath = self::COMPONENTS_DIR . DIRECTORY_SEPARATOR . $view . DIRECTORY_SEPARATOR . 'Resources' . DIRECTORY_SEPARATOR . $matchesResources[1][$i] . DIRECTORY_SEPARATOR;
+                $resources = scandir($resourcesPath);
+                $file = $nameResource[$i].".{$matchesResources[1][$i]}";
+                $found = in_array($file, $resources);
+
+                if ($found){
+                    $path = "'{$resourcesPath}{$file}'";
+                    $this->currentComponent = str_replace($matchesResources[0][$i], $path, $this->currentComponent);
+                }
+            }
+        }
     }
 }
