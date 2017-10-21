6c77906ad464d5bb2ff83f45ebdf2926f07fd84f
Mario Henmanuel VU <hemma.hvu@gmail.com>
2017-10-21 13:44:19 -0600

    Loop Rest System Views

--- views/Expected_Views.class.php
+++ views/Expected_Views.class.php
@@ -7,7 +7,7 @@ class Expected_Views{
     const NOT_FOUND_COMP = "<!-- {file} not fount -->";
     const PARENT_EXPLODE = '/[a-zA-Z0-9_\-]:[a-zA-Z0-9_\-]/';
 
-    const COMPS_DIR = 'views' . DS . 'staticFiles';
+    const COMPS_DIR = 'views' . DS . 'staticFiles' . DS . 'www';
     const COMP_INIT = '/<comp[\s]*name=["|\']/';
     const COMP_END = '/["|\']+[\s]*\/>/';
     const COMP_RGX = '/<comp[\s]*name=["|\'][a-zA-Z0-9_\-]*["|\']+[\s]*\/>/';
