1e2ecf06cb0baf636c77a4151e7180358f6da8c4
Mario Henmanuel VU <hemma.hvu@gmail.com>
2017-11-19 14:11:17 -0600

    Names Repair

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 /*
 |---------------------------------------------------------------------------
 |Web services REST Loop in server PHP
